490bcc8a7c316d2a3cc293c3b57b6044c2a94f0a
betsyvies <ing.betsyvies@gmail.com>
2017-11-18 17:15:30 -0500

    añadiendo avance de V.0.0.3

--- js/app.js
+++ js/app.js
@@ -10,7 +10,9 @@ window.addEventListener('load', function() {
 function insertText(event) {
   event.preventDefault();  
 
-  if (textTarea.value !== '') {
+  if (textTarea.value === '' && input.value > 140) {
+    event.disabled = true;
+  } else {
     var containerParagraph = document.getElementById('second-section');
     var paragraph = document.createElement('div');
     paragraph.className = 'paragraph';
@@ -26,14 +28,18 @@ function insertText(event) {
     textTarea.value = '';
     input.value = '';
 
-    event.target.disabled = false;
-  } else {
-    event.target.disabled = true;
+    event.disabled = false;
   }
 }
 
 function accountant(event) {
-  var input = document.getElementById('input');
-  input.value = 140;
-  input.value = input.value - textTarea.value.length;
+  if (textTarea.value.length) {
+    var input = document.getElementById('input');
+    input.value = 140;
+    input.value = input.value - textTarea.value.length;
+    input.classList.toggle('blue', 130 < textTarea.value.length <= 140);
+    input.classList.toggle('brown', 120 <= textTarea.value.length <= 130); 
+  } 
 };
+
+
