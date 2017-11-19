8bb5e39ab460171dcf6bf48dc8dfd25c73f5c697
betsyvies <ing.betsyvies@gmail.com>
2017-11-19 00:44:52 -0500

    V.0.0.3 terminado

--- js/app.js
+++ js/app.js
@@ -1,45 +1,50 @@
+/* Inicializamos con una funcion que contendra los eventos a utilizar */
 window.addEventListener('load', function() {
   var textTarea = document.getElementById('textTarea');
   textTarea.addEventListener('keydown', accountant);
-  textTarea.addEventListener('keyup', accountant);
   var button = document.getElementById('button');
   button.addEventListener('click', insertText);
   button.addEventListener('click', accountant);
+  submit.disabled = true;
 });
 
+/* La funcion insertText permite ingresar un texto a un contenedor */
 function insertText(event) {
   event.preventDefault();  
 
-  if (textTarea.value === '' && input.value > 140) {
-    event.disabled = true;
-  } else {
+  if (textTarea.value.length > 0) {
+    button.disabled = false;
+
     var containerParagraph = document.getElementById('second-section');
     var paragraph = document.createElement('div');
     paragraph.className = 'paragraph';
-  
+      
     var text = document.createElement('p');
     text.className = 'text';
-    
+        
     text.textContent = textTarea.value;
-       
+           
     paragraph.appendChild(text);  
     containerParagraph.appendChild(paragraph);
-
+    
     textTarea.value = '';
     input.value = '';
-
-    event.disabled = false;
+  } else {
+    button.disabled = true;
   }
 }
 
+/* La funcion accountant cuenta la cantidad de caracteres de forma regresiva */
+
 function accountant(event) {
-  if (textTarea.value.length) {
-    var input = document.getElementById('input');
-    input.value = 140;
-    input.value = input.value - textTarea.value.length;
-    input.classList.toggle('blue', 130 < textTarea.value.length <= 140);
-    input.classList.toggle('brown', 120 <= textTarea.value.length <= 130); 
+  button.disabled = false;
+  var input = document.querySelector('.container');
+  input.value = 140 - textTarea.value.length;
+  if (input.value < 0) {
+    button.disabled = true;
   } 
+  input.classList.toggle('blue', 10 < input.value && input.value <= 20);
+  input.classList.toggle('red', input.value <= 10);
 };
 
 
